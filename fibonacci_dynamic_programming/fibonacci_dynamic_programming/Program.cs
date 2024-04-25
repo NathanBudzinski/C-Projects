a6caf7ca9ea4ed5f4e29d4fcbe629086b4b0fbb1
Nathan Budzinski <107956972+natebud30@users.noreply.github.com>
2024-04-25 14:58:15 -0500

    Final Polishing
    Removed unnecessary whitespace and joined 2 conditionals into a single conditional for simplicity.

--- fibonacci_dynamic_programming/fibonacci_dynamic_programming/Program.cs
+++ fibonacci_dynamic_programming/fibonacci_dynamic_programming/Program.cs
@@ -1,24 +1,17 @@
-﻿
 class Fib
 {
-
     public static double Fibonacci(double n)
     {
-        if (n == 1) return 1;
-        if (n == 2) return 1;
+        if (n == 1 || n == 2) return 1;
         double[] M = new double[(int)n];
         return Fibonacci_Recursive(n-1, M) + Fibonacci_Recursive(n-2, M);
     }
-
     public static double Fibonacci_Recursive(double n, double[] M)
     {
         if (M[(int)n] > 0) return M[(int)n];
         if (n == 1 || n == 2) return 1;
         return M[(int)n] = Fibonacci_Recursive(n - 1, M) + Fibonacci_Recursive(n - 2, M);
     }
-
-
-
     public static void Main()
     {
         Console.WriteLine("Calculate Fibonacci number: ");
